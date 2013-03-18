dfaec0513d1065e19457c6ee44d2ce55933aa9f4
Summers Pittman <supittma@redhat.com>
2013-03-18 16:37:49 -0400

    Extend FragmentActivity

--- src/org/jboss/aerogear/todo/activities/LoginActivity.java
+++ src/org/jboss/aerogear/todo/activities/LoginActivity.java
@@ -20,16 +20,16 @@ package org.jboss.aerogear.todo.activities;
 import org.jboss.aerogear.todo.R;
 import org.jboss.aerogear.todo.ToDoApplication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager.LayoutParams;
 import android.widget.EditText;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
 
 	protected static final String TAG = LoginActivity.class.getSimpleName();
 
